5869e50a9d1532b4976ec7445b1d86fae553e59c
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-10-13 12:15:40 -0300

    exercicio 04

---
+++ Exercicio_04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio_04 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o texto: ");
+            string texto = Console.ReadLine();
+            int contador = 0;           
+            for (int i = 0; i < texto.Length; i++) {
+                if (texto[i] == ' ') {
+                    continue;
+                }
+                contador++;
+            }
+            Console.WriteLine($"Total de caracteres: {contador}");
+        }
+    }
+}
