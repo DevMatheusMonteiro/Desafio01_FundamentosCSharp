72da7b2b324c3fe13ea368c5557ec022db7fb61d
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-10-13 11:27:32 -0300

    exercicio 02

---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio_02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Entre com o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Entre com o seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+            string nomeCompleto = nome + " " + sobrenome;
+            Console.WriteLine(nomeCompleto);
+        }
+    }
+}
