9586ed4f4779c763889b30817405e1fef19cb504
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-10-13 15:06:13 -0300

    exercicio 5

---
+++ Exercicio_05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio_05 {
+    internal class Program {
+        static void Main(string[] args) {
+            string placa = Console.ReadLine();
+            bool valido = true;
+            if (string.IsNullOrEmpty(placa) || placa.Length != 7) {
+                valido = false;
+            } else { 
+                string tresPrimeiros = placa.Substring(0, 3);
+                string quatroUltimos = placa.Substring(3);
+                for (int i = 0; i < tresPrimeiros.Length; i++) {
+                    if (!char.IsLetter(tresPrimeiros[i])) {
+                        valido = false;
+                        break;
+                    }
+                }
+                for (int i = 0; i < quatroUltimos.Length; i++) {
+                    if (!char.IsNumber(quatroUltimos[i])) {
+                        valido = false;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(valido ? "Placa Válida": "Placa Inválida");
+        }
+    }
+}
