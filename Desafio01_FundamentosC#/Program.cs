18e7af9e68e5415a0ca5b49f02e94b81b1963953
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-10-13 11:16:45 -0300

    exercicio 01

---
+++ Desafio01_FundamentosC#/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Desafio01_FundamentosC_ {
+    internal class Program {
+        static void Main(string[] args) {
+            string? nome = EntrarNome("Entre com seu nome: ");
+            BoasVindas(nome);
+        }
+        static string? EntrarNome(string mensagem) {
+            Console.Write(mensagem);
+            return Console.ReadLine();
+        }
+        static void BoasVindas(string? nome) {
+            if (string.IsNullOrEmpty(nome)) {
+                Console.WriteLine("Erro: nome inválido!");
+                return;
+            }
+            Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo(a)");
+        }
+    }
+}
