ed4cab3689110db84dd43618c3f77c4a4d8a1072
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-10-13 15:52:13 -0300

    exercicio 06

---
+++ Exercicio_06/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Exercicio_06 {
+    internal class Program {
+        static void Main(string[] args) {
+            DateTime hoje = DateTime.UtcNow.AddHours(-3);
+            CultureInfo culture = new CultureInfo("pt-BR");
+            int escolha;
+            do {
+                Console.Write("Escolha um formato da exibição da data:\n\n" +
+                    "1 - Formato Completo\n" +
+                    "2 - Formato dd/MM/yyyy\n" +
+                    "3 - Apenas a hora no formato 24 horas\n" +
+                    "4 - Data com mês por extenso\n" +
+                    "0 - Encerrar\n");
+                escolha = EntrarNumero();
+                switch (escolha) {
+                    case 1:
+                    Console.WriteLine(hoje.ToString("F", culture));
+                    break;
+                    case 2:
+                    Console.WriteLine(hoje.ToString("dd/MM/yyyy", culture));
+                    break;
+                    case 3:
+                    Console.WriteLine(hoje.ToString("HH:mm:ss", culture));
+                    break;
+                    case 4:
+                    Console.WriteLine(hoje.ToString("dd-MMMM-yyyy", culture));
+                    break;
+                    case 0:
+                    Console.WriteLine("Encerrado!");
+                    break;
+                    default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+                }
+            }
+            while (escolha != 0);
+        }
+        static int EntrarNumero() {
+            do {
+                try {
+                    return int.Parse(Console.ReadLine());
+                } catch {
+                    Console.WriteLine("Erro: número inválido");
+                }
+            } while (true);
+        }
+    }
+}
